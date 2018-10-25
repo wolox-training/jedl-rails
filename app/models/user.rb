3b73482bcea914ed77c845026225422f0e50e71f
Jorge Diaz <jorge.diaz@wolox.co>
2018-10-25 09:23:31 -0500

    UPDATE app/models/user.rb rubocop fix, to new syntax when validate password confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates_confirmation_of :password
+  validates :password, confirmation: true
 end
