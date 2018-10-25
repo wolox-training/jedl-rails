f850c6049ab189715ce82f6a6ba5c7185bf95386
Jorge Diaz <jorge.diaz@wolox.co>
2018-10-25 15:29:20 -0500

    UPDATE Gemfile/Gemfile.lock devise_toke_auth installation
    app/controllers/application_controller.rb
    app/models/user.rb
    
    DELETED
    db/migrate/20181025132807_devise_create_users.rb
    delete unused model
    
    CREATED
    db/migrate/20181025153846_devise_token_auth_create_users.rb
    new model to user auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :trackable, :validatable
+
+  include DeviseTokenAuth::Concerns::User
 
   validates :password, confirmation: true
 end
