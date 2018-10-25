ed5a548e1bcb2c2bdb285a3c41ac8fa08b2c8135
Jorge Diaz <jorge.diaz@wolox.co>
2018-10-25 15:29:20 -0500

    UPDATE Gemfile/Gemfile.lock devise_toke_auth installation
    app/controllers/application_controller.rb
    app/models/user.rb
    
    DELETED
    db/migrate/20181025132807_devise_create_users.rb
    delete unused model
    
    CREATED
    db/migrate/20181025153846_devise_token_auth_create_users.rb
    new model to user auth

--- db/migrate/20181025153846_devise_token_auth_create_users.rb
+++ db/migrate/20181025153846_devise_token_auth_create_users.rb
@@ -8,7 +8,7 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.2]
 
       ## Database authenticatable
       t.string :encrypted_password, null: false, default: ""
-
+      
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
