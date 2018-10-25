24fa2be7544e2aa1cc9872f76e15ad458b2b1305
Jorge Diaz <jorge.diaz@wolox.co>
2018-10-25 16:37:17 -0500

    UPDATE app/models/book.rb rubocop fix on spaces

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,9 @@
 class Book < ApplicationRecord
-    validates :genre, presence: true
-    validates :author, presence: true
-    validates :image, presence: true
-    validates :title, presence: true
-    validates :editor, presence: true
-    validates :year, presence: true, length: {minimum: 4, maximum: 4}, numericality: { only_integer: true }
+  validates :genre, presence: true
+  validates :author, presence: true
+  validates :image, presence: true
+  validates :title, presence: true
+  validates :editor, presence: true
+  validates :year, presence: true, length: { minimum: 4, maximum: 4 },
+                   numericality: { only_integer: true }
 end
