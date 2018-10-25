08759b04d7bafe7ba5dc4a31f2fea93e70130725
Jorge Diaz <jorge.diaz@wolox.co>
2018-10-25 15:37:34 -0500

    UPDATE README.md how to run unit test
    app/models/user.rb
    rubocop fix, ruby style

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable,
+         :validatable
 
   include DeviseTokenAuth::Concerns::User
 
