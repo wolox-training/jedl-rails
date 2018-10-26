bb561b7a2b4043b9312d837b02caa9c84aafbfee
Jorge Diaz <jorge.diaz@wolox.co>
2018-10-26 12:22:30 -0500

    UPDATE rubocop fix

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,13 +1,13 @@
 require 'faker'
 
 FactoryBot.define do
-    factory :user, class: User do
-        password = Faker::Internet.password(8)
+  factory :user, class: User do
+    password = Faker::Internet.password(8)
 
-        first_name { Faker::Name.first_name }
-        last_name { Faker::Name.last_name }
-        email { Faker::Internet.email }
-        password { password }
-        password_confirmation { password }
-    end
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email { Faker::Internet.email }
+    password { password }
+    password_confirmation { password }
+  end
 end
