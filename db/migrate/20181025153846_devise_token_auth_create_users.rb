e34eb4b4e88c858e8f1221ec8585c64ad958c0a7
Jorge Diaz <jorge.diaz@wolox.co>
2018-10-29 16:14:00 -0500

    UPDATE remove unnecesary comments

--- db/migrate/20181025153846_devise_token_auth_create_users.rb
+++ db/migrate/20181025153846_devise_token_auth_create_users.rb
@@ -30,15 +30,7 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.2]
       t.datetime :confirmation_sent_at
       t.string   :unconfirmed_email # Only if using reconfirmable
 
-      ## Lockable
-      # t.integer  :failed_attempts, :default => 0, :null => false # Only if lock strategy is :failed_attempts
-      # t.string   :unlock_token # Only if unlock strategy is :email or :both
-      # t.datetime :locked_at
-
-      ## User Info
-      # t.string :name
-      # t.string :nickname
-      # t.string :image
+      ## User info
       t.string :email, null: false, default: ""
 
       t.string :first_name, null: false, default: ""
