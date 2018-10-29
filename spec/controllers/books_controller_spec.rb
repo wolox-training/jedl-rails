f46d72f8362c31148c7dae962900a007010e1340
Jorge Diaz <jorge.diaz@wolox.co>
2018-10-29 15:57:07 -0500

    UPDATE rubocop fix

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe API::V1::BooksController, type: :controller do
-
 end
--- spec/factories/rent_factory.rb
+++ spec/factories/rent_factory.rb
@@ -1,10 +1,10 @@
 require 'faker'
 
 FactoryBot.define do
-    factory :rent, class: Rent do
-        start_date { Time.now }
-        end_date { Time.now + (60 * 60 * 24)}
-        user
-        book
-    end
+  factory :rent, class: Rent do
+    start_date { Time.zone.now }
+    end_date { Time.zone.now + (60 * 60 * 24) }
+    user
+    book
+  end
 end
