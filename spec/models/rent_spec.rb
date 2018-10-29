c16232cba1ee8a923febb8957640d325b5f2e6f5
Jorge Diaz <jorge.diaz@wolox.co>
2018-10-29 11:44:26 -0500

    UPDATE test to save rent with valid and invalid params

--- spec/models/rent_spec.rb
+++ spec/models/rent_spec.rb
@@ -5,4 +5,20 @@ describe Rent do
 
   it { is_expected.to validate_presence_of(:start_date) }
   it { is_expected.to validate_presence_of(:end_date) }
+
+  describe '#save' do
+    context 'With valid params' do
+      it 'returns the new rent object' do
+        rent.save!
+        expect(rent).to be_present
+      end
+    end
+
+    let(:emptyRent) { Rent.new }
+    context 'With invalid params' do
+      it 'returns exception' do
+        expect { emptyRent.save! }.to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+  end
 end
