59cb1611d31b1fd3f68eeda0a4efe676c1ef417e
Jorge Diaz <jorge.diaz@wolox.co>
2018-10-30 06:54:01 -0500

    UPDATE remove unnecessary comments

--- db/migrate/20181025153846_devise_token_auth_create_users.rb
+++ db/migrate/20181025153846_devise_token_auth_create_users.rb
@@ -46,6 +46,5 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.2]
     add_index :users, [:uid, :provider],     unique: true
     add_index :users, :reset_password_token, unique: true
     add_index :users, :confirmation_token,   unique: true
-    # add_index :users, :unlock_token,       unique: true
   end
 end
