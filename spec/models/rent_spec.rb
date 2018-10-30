64c34ea95b676653bd15c6bf7c1259bf67cdd805
Jorge Diaz <jorge.diaz@wolox.co>
2018-10-30 10:06:02 -0500

    UPDATE app/models/rent.rb include validation created
    app/validators/start_end_date_validator.rb
    moved a validator into its own class
    
    db/migrate/20181026173627_create_rents.rb
    include reference on the definition of the table
    
    spec/models/rent_spec.rb
    - include validation of each field
    - change expect to "be invalid"

--- spec/models/rent_spec.rb
+++ spec/models/rent_spec.rb
@@ -5,6 +5,8 @@ describe Rent do
 
   it { is_expected.to validate_presence_of(:start_date) }
   it { is_expected.to validate_presence_of(:end_date) }
+  it { is_expected.to validate_presence_of(:book) }
+  it { is_expected.to validate_presence_of(:user) }
 
   describe '#save' do
     context 'With valid params' do
@@ -14,10 +16,25 @@ describe Rent do
       end
     end
 
-    let(:emptyRent) { Rent.new }
     context 'With invalid params' do
-      it 'returns exception' do
-        expect { emptyRent.save! }.to raise_error(ActiveRecord::RecordInvalid)
+      let(:withoutBookValue) { build(:rent, book: nil) }
+      it 'returns invalid book value' do
+        expect(withoutBookValue).to be_invalid
+      end
+
+      let(:withoutUserValue) { build(:rent, user: nil) }
+      it 'returns invalid user value' do
+        expect(withoutUserValue).to be_invalid
+      end
+
+      let(:withoutStartDateValue) { build(:rent, start_date: "") }
+      it 'returns invalid start date value' do
+        expect(withoutStartDateValue).to be_invalid
+      end
+
+      let(:withoutEndDateValue) { build(:rent, end_date: "") }
+      it 'returns invalid end date value' do
+        expect(withoutEndDateValue).to be_invalid
       end
     end
   end
