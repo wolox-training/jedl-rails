d61660c15f161461baaea6f7561dd0162ab92b5a
Jorge Diaz <jorge.diaz@wolox.co>
2018-10-30 14:27:41 -0500

    UPDATE app/controllers/api/v1/books_controller.rb change render of method show when not found a book
    spec/controllers/api/v1/books_controller_spec.rb
    test of pagination, test when a book to show doesn't exist

--- spec/controllers/api/v1/books_controller_spec.rb
+++ spec/controllers/api/v1/books_controller_spec.rb
@@ -79,6 +79,56 @@ end
 describe API::V1::BooksController, type: :controller do
   include_context 'Authenticated User'
 
+  describe 'GET #index' do
+    context 'When fetching all the books paginated' do
+      let!(:books) { create_list(:book, 28) }
+
+      before do
+        get :index, params: { page: 2 }
+      end
+
+      it 'responses with the books json' do
+        expected = ActiveModel::Serializer::CollectionSerializer.new(
+          books, each_serializer: BookSerializer
+        ).to_json
+        expect(response.body.to_json) =~ JSON.parse(expected)
+      end
+
+      it 'responds with 200 status' do
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+end
+
+describe API::V1::BooksController, type: :controller do
+  include_context 'Authenticated User'
+
+  describe 'GET #index' do
+    context 'When fetching all the books paginated by inexistent page' do
+      let!(:books) { create_list(:book, 2) }
+
+      before do
+        get :index, params: { page: 2 }
+      end
+
+      it 'responses with the empty pager json' do
+        expected = ActiveModel::Serializer::CollectionSerializer.new(
+          books, each_serializer: BookSerializer
+        ).to_json
+        expect(response.body.to_json) =~ JSON.parse(expected)
+      end
+
+      it 'responds with 200 status' do
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+end
+
+describe API::V1::BooksController, type: :controller do
+  include_context 'Authenticated User'
+
   describe 'GET #show' do
     context 'When fetching a book' do
       let!(:book) { create(:book) }
@@ -91,6 +141,22 @@ describe API::V1::BooksController, type: :controller do
         expect(response.body).to eq Books::ShowSerializer.new(
           book, root: false
         ).to_json
+      end
+
+      it 'responds with 200 status' do
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'When fetching an inexistent book' do
+      #let!(:book) { create(:book) }
+
+      before do
+        get :show, params: { id: 1 }
+      end
+
+      it 'responses with the book json' do
+        expect(response.body).to eq ({ error: "Book doesn't found" }).to_json
       end
 
       it 'responds with 200 status' do
