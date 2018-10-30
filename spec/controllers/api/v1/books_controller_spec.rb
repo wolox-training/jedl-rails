1bafe89dc072f4d94608f4031b20c30b92bc9a9a
Jorge Diaz <jorge.diaz@wolox.co>
2018-10-30 14:27:41 -0500

    UPDATE app/controllers/api/v1/books_controller.rb change render of method show when not found a book
    spec/controllers/api/v1/books_controller_spec.rb
    test of pagination, test when a book to show doesn't exist

--- spec/controllers/api/v1/books_controller_spec.rb
+++ spec/controllers/api/v1/books_controller_spec.rb
@@ -97,20 +97,6 @@ describe API::V1::BooksController, type: :controller do
         expect(response).to have_http_status(:ok)
       end
     end
-
-    context 'When fetching an inexistent book' do
-      before do
-        get :show, params: { id: 1 }
-      end
-
-      it 'responses with the book json' do
-        expect(response.body).to eq ({ error: "Book doesn't found" }).to_json
-      end
-
-      it 'responds with 200 status' do
-        expect(response).to have_http_status(:ok)
-      end
-    end
   end
 end
 
