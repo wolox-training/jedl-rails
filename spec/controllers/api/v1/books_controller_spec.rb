d3d5d8b3d8e66fecd2bea5790f0f1d42dd4e054c
Jorge Diaz <jorge.diaz@wolox.co>
2018-10-31 08:30:33 -0500

    change rescue for rescue_from in controllers, shared_context put it in a helper

--- spec/controllers/api/v1/books_controller_spec.rb
+++ spec/controllers/api/v1/books_controller_spec.rb
@@ -79,56 +79,6 @@ end
 describe API::V1::BooksController, type: :controller do
   include_context 'Authenticated User'
 
-  describe 'GET #index' do
-    context 'When fetching all the books paginated' do
-      let!(:books) { create_list(:book, 28) }
-
-      before do
-        get :index, params: { page: 2 }
-      end
-
-      it 'responses with the books json' do
-        expected = ActiveModel::Serializer::CollectionSerializer.new(
-          books, each_serializer: BookSerializer
-        ).to_json
-        expect(response.body.to_json) =~ JSON.parse(expected)
-      end
-
-      it 'responds with 200 status' do
-        expect(response).to have_http_status(:ok)
-      end
-    end
-  end
-end
-
-describe API::V1::BooksController, type: :controller do
-  include_context 'Authenticated User'
-
-  describe 'GET #index' do
-    context 'When fetching all the books paginated by inexistent page' do
-      let!(:books) { create_list(:book, 2) }
-
-      before do
-        get :index, params: { page: 2 }
-      end
-
-      it 'responses with the empty pager json' do
-        expected = ActiveModel::Serializer::CollectionSerializer.new(
-          books, each_serializer: BookSerializer
-        ).to_json
-        expect(response.body.to_json) =~ JSON.parse(expected)
-      end
-
-      it 'responds with 200 status' do
-        expect(response).to have_http_status(:ok)
-      end
-    end
-  end
-end
-
-describe API::V1::BooksController, type: :controller do
-  include_context 'Authenticated User'
-
   describe 'GET #show' do
     context 'When fetching a book' do
       let!(:book) { create(:book) }
