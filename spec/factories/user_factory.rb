d4cac57fad19afd1a0b20e19cac83ccd4dcd9e04
Jorge Diaz <jorge.diaz@wolox.co>
2018-10-31 10:24:44 -0500

    change faker and factory_bot_rails gems into development and test environment, remove variable password and put data field password into password_confirmation, remove unnecesary test

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -2,12 +2,10 @@ require 'faker'
 
 FactoryBot.define do
   factory :user, class: User do
-    password = Faker::Internet.password(8)
-
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     email { Faker::Internet.email }
-    password { password }
+    password { Faker::Internet.password(8) }
     password_confirmation { password }
   end
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -13,23 +13,3 @@ describe Book do
   it { is_expected.to validate_numericality_of(:year).only_integer }
   it { is_expected.to validate_length_of(:year).is_equal_to(4) }
 end
-
-describe Book do
-  subject(:book) { build(:book) }
-
-  describe '#save' do
-    context 'With valid params' do
-      it 'returns the new book object' do
-        book.save!
-        expect(book).to be_present
-      end
-    end
-
-    let(:emptyBook) { Book.new }
-    context 'With invalid params' do
-      it 'returns exception' do
-        expect { emptyBook.save! }.to raise_error(ActiveRecord::RecordInvalid)
-      end
-    end
-  end
-end
