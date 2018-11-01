24286415778c738b271eddfab156016e80c86402
Jorge Diaz <jorge.diaz@wolox.co>
2018-11-01 16:19:09 -0500

    rubocop fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,34 +3,13 @@ class User < ApplicationRecord
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-<<<<<<< HEAD
-<<<<<<< HEAD
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable,
          :validatable
-=======
-  devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable
-=======
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable,
-         :validatable
->>>>>>> UPDATE
-
-  include DeviseTokenAuth::Concerns::User
->>>>>>> UPDATE
 
   validates :email, uniqueness: true, email: true
   validates :password, confirmation: true
   validates :first_name, :last_name, presence: true
-<<<<<<< HEAD
-<<<<<<< HEAD
 
   has_many :rents, dependent: :destroy
   has_many :books, through: :rents
-=======
->>>>>>> UPDATE
-=======
-
-  has_many :rents, dependent: :destroy
-  has_many :books, through: :rents
->>>>>>> UPDATE
 end
