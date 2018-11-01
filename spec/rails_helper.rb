502224ece20609bd451b12caef5d94deaca6c7e6
Jorge Diaz <jorge.diaz@wolox.co>
2018-11-01 12:53:35 -0500

    changes on rebase of master

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,15 +1,9 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
-<<<<<<< HEAD
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
-=======
-require File.expand_path('../../config/environment', __FILE__)
-# Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
->>>>>>> UPDATE
 require 'rspec/rails'
 require 'support/factory_bot.rb'
 # Add additional requires below this line. Rails is not loaded until this point!
@@ -45,10 +39,6 @@ Shoulda::Matchers.configure do |config|
   end
 end
 
-<<<<<<< HEAD
-=======
-
->>>>>>> UPDATE
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
