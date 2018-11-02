6d9ec30692854fc7ba045b602ef3dff7b4193fea
Jorge Diaz <jorge.diaz@wolox.co>
2018-11-02 16:15:28 -0500

    change the way of create new rent object

--- app/controllers/api/v1/rents_controller.rb
+++ app/controllers/api/v1/rents_controller.rb
@@ -3,36 +3,22 @@ module API
     class RentsController < ApplicationController
       include Wor::Paginate
 
-      before_action :authenticate_user!, :find_user
-
-      @user = nil
+      before_action :authenticate_user!
 
       def index
-        rents = Rent.where(user: @user)
+        rents = Rent.where(user: params[:user_id])
         render_paginated rents, each_serializer: Rents::IndexSerializer
       end
 
       def create
-        rent = Rent.new(parse_create_rent)
+        rent = Rent.new(params.require(:rent)
+                        .permit(:start_date, :end_date, :book_id, :user_id, :returned_at))
 
         if rent.save
           render json: rent, serializer: Rents::CreateSerializer, status: :created
         else
           render json: rent.errors
         end
-      end
-
-      private
-
-      def find_user
-        @user = User.find(params[:user_id])
-      end
-
-      def parse_create_rent
-        rent = params[:rent].clone
-        rent[:start_date] = rent.delete :from
-        rent[:end_date] = rent.delete :to
-        rent.permit!
       end
     end
   end
