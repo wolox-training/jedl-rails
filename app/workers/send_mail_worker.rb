757aab37be5869a3b925fa06b1b0afc0b4076473
Jorge Diaz <jorge.diaz@wolox.co>
2018-11-06 10:21:48 -0500

    remove unnecessary comments

--- app/workers/send_mail_worker.rb
+++ app/workers/send_mail_worker.rb
@@ -3,7 +3,6 @@ class SendMailWorker
 
     def perform(rent_id)
         puts "Sending email..."
-        #RentCreatedMailer.with(current_rent).created
         RentCreatedMailer.created(rent_id).deliver_now
         puts "Finish."
     end
