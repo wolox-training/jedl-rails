3709b2c398174f333da4dcb8770f46ee56f29a33
Jorge Diaz <jorge.diaz@wolox.co>
2018-11-07 07:48:41 -0500

    add locale field to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ class User < ApplicationRecord
   validates :password, confirmation: true
   validates :first_name, :last_name, presence: true
 
+  enum locale: { es: "es", en: "en" }
+
   has_many :rents, dependent: :destroy
   has_many :books, through: :rents
 end
