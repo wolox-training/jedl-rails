7034486edac2375c0b633d3291114f3433c52f50
Jorge Diaz <jorge.diaz@wolox.co>
2018-11-07 11:02:41 -0500

    rubocop fix

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,11 +1,11 @@
 class ApiController < ActionController::Base
-	include DeviseTokenAuth::Concerns::SetUserByToken
+  include DeviseTokenAuth::Concerns::SetUserByToken
 
-	rescue_from ActiveRecord::RecordNotFound do |exception|
-		render json: { error: exception.message }, status: :not_found
-	end
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    render json: { error: exception.message }, status: :not_found
+  end
 
-	rescue_from ActiveRecord::RecordInvalid do |exception|
-		render json: { error: exception.message }, status: :bad_request
-	end
+  rescue_from ActiveRecord::RecordInvalid do |exception|
+    render json: { error: exception.message }, status: :bad_request
+  end
 end
