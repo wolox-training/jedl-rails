b69e030b477221ca77a0e21edb1caebb47289462
Jorge Diaz <jorge.diaz@wolox.co>
2018-11-08 11:39:48 -0500

    include forgery in api_controller

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,8 @@
 class ApiController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
 
+  protect_from_forgery with: :null_session
+
   rescue_from ActiveRecord::RecordNotFound do |exception|
     render json: { error: exception.message }, status: :not_found
   end
